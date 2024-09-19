a1b4f0b383710b4d759f768b7a15b636a089d095
kevintv789 <kevintv789@gmail.com>
2024-09-18 22:45:21 -0700

    created foyer level

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,14 +2,21 @@ extends CharacterBody2D
 
 @export var speed: int = 400
 
+@onready var player_light: Light2D = $PlayerSprite/PlayerLight
+
 func _ready() -> void:
 	# Set flashlight to not visible
-	$Flashlight.visible = false
+	initialize_player_light()
 
 func _physics_process(_delta: float) -> void:
 	define_input_map()
 	follow_mouse()
 	move_and_slide()
+
+func initialize_player_light() -> void:
+	$Flashlight.visible = false
+	player_light.energy = 0.1
+	player_light.texture_scale = 0.3
 
 func _input(event: InputEvent) -> void:
 	toggle_flashlight(event)
@@ -21,7 +28,9 @@ func define_input_map() -> void:
 func toggle_flashlight(event: InputEvent) -> void:
 	if event.is_action_pressed(InputManager.FLASHLIGHT_TOGGLE):
 		$Flashlight.visible = !$Flashlight.visible
-
+		player_light.energy = 1.0 if $Flashlight.visible else 0.5
+		player_light.texture_scale = 1.0 if $Flashlight.visible else 0.3
+		
 func follow_mouse() -> void:
 	var mouse_position = get_global_mouse_position()
 	look_at(mouse_position)
