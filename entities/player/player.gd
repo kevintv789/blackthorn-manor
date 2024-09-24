c8784fa0c0a6a86eaf3f4a62f48db17dfa55afc1
kevintv789 <kevintv789@gmail.com>
2024-09-24 09:58:10 -0700

    updated player sprite with clothes and flashlight

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -7,27 +7,23 @@ class_name Player
 
 @onready var player_sprite: Sprite2D = $SmoothNode/PlayerSprite
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var flashlight_sprite: PointLight2D = $SmoothNode/PlayerSprite/Flashlight
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 @onready var prompt_button_scene: PackedScene = preload("res://scenes/UI/e_button.tscn")
+
+var base_sprite_path: String = "res://assets/2D/sprites/player/base_male.png"
+var flashlight_sprite_path: String = "res://assets/2D/sprites/player/male_flashlight.png"
 
 var prompt_button: Node2D
 
 var current_input_direction = Vector2.RIGHT
 
 func _ready() -> void:
-	prompt_button = prompt_button_scene.instantiate()
-	add_child(prompt_button)
-	prompt_button.visible = false
-	prompt_button.scale = Vector2(1.5, 1.5)
+	InputManager.flashlight_toggle.connect(_on_flashlight_toggle)
+	initialize_item_pickup_prompt()
+	initialize_item_pickup_signals()
 
-	var items = get_tree().get_nodes_in_group("Items") as Array[Node2D]
-	for item in items:
-		if item.has_signal("in_range"):
-			item.connect("in_range", key_in_range)
-		if item.has_signal("out_of_range"):
-			item.connect("out_of_range", key_out_of_range)
-	
 func _physics_process(delta: float) -> void:
 	define_input_map()
 	light_follow_mouse(delta)
@@ -37,6 +33,20 @@ func _physics_process(delta: float) -> void:
 	if prompt_button:
 		prompt_button.global_position = global_position + Vector2(0, -50)
 		prompt_button.rotation = -rotation
+
+func initialize_item_pickup_prompt() -> void:
+	prompt_button = prompt_button_scene.instantiate()
+	add_child(prompt_button)
+	prompt_button.visible = false
+	prompt_button.scale = Vector2(1.5, 1.5)
+
+func initialize_item_pickup_signals() -> void:
+	var items = get_tree().get_nodes_in_group("Items") as Array[Node2D]
+	for item in items:
+		if item.has_signal("in_range"):
+			item.connect("in_range", key_in_range)
+		if item.has_signal("out_of_range"):
+			item.connect("out_of_range", key_out_of_range)
 
 func define_input_map() -> void:
 	var input_direction = Input.get_vector(InputManager.MOVE_LEFT, InputManager.MOVE_RIGHT, InputManager.MOVE_UP, InputManager.MOVE_DOWN)
@@ -54,7 +64,7 @@ func light_follow_mouse(delta: float) -> void:
 	var angle_to_mouse = global_position.direction_to(mouse_position).angle()
 
 	# Snap to 8 directions (45-degree increments)
-	var snapped_angle = round(angle_to_mouse / (PI/4)) * (PI/4)
+	var snapped_angle = round(angle_to_mouse / (PI / 4)) * (PI / 4)
 
 	# Smoothly interpolate the rotation, but slower
 	rotation = lerp_angle(rotation, snapped_angle, 5 * delta)
@@ -77,3 +87,9 @@ func key_in_range() -> void:
 
 func key_out_of_range() -> void:
 	prompt_button.visible = false
+
+func _on_flashlight_toggle(is_on: bool) -> void:
+	if is_on:
+		flashlight_sprite.offset = Vector2(480, 0)
+
+	player_sprite.texture = load(flashlight_sprite_path) if is_on else load(base_sprite_path)
--- globals/input_manager.gd
+++ globals/input_manager.gd
@@ -7,3 +7,11 @@ const MOVE_UP = "move_up"
 const MOVE_DOWN = "move_down"
 const ITEM_PICKUP = "item_pickup"
 const FLASHLIGHT_TOGGLE = "flashlight_toggle"
+
+# Signals
+signal flashlight_toggle(is_on)
+
+var is_flashlight_on: bool = false:
+	set(value):
+		is_flashlight_on = value
+		flashlight_toggle.emit(is_flashlight_on)
