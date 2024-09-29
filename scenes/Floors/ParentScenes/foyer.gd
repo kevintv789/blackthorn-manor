a6c785f2284e9e089fd129b57437fb455aa09209
kevintv789 <kevintv789@gmail.com>
2024-09-29 00:02:08 -0700

    randomized monster spawn points on key pickup

---
+++ scenes/Floors/ParentScenes/foyer.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var monster_spawn_points: Node2D = $MonsterSpawnPoints
+
+func _ready() -> void:
+	var items = get_tree().get_nodes_in_group("Items") as Array[Node2D]
+	for item in items:
+		if item.has_signal("picked_up"):
+			item.connect("picked_up", spawn_monster)
+
+func spawn_monster(_item: Node2D) -> void:
+	var monster = preload("res://scenes/Enemies/monster.tscn").instantiate()
+	monster.player = get_tree().get_first_node_in_group("Player")
+	var spawn_points = monster_spawn_points.get_children() as Array[Marker2D]
+	var random_spawn_point = spawn_points.pick_random()
+	monster.global_position = random_spawn_point.global_position
+	add_child(monster)
